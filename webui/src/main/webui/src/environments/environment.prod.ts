ced5241111d2b30bfc01203382b48d8b06f01aff
Mohamed ELOUASTANI <moha.elouastani@gmail.com>
2020-04-27 17:38:46 +0000

    Fix? env prod

--- webui/src/main/webui/src/environments/environment.prod.ts
+++ webui/src/main/webui/src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  endpoint: 'http://localhost:8080/esky'
+};
+
+export const api = {
+  course: '/course',
+  save: '/save',
+  get: '/get',
+  update: '/update',
+  delete: '/delete',
+  all: '/all',
+  auth: '/auth',
+  filter: '/filter',
+  count: '/count'
 };

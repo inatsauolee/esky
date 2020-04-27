83145149d5d442f8831e03f1fb1fab736d6d6521
Mohamed ELOUASTANI <moha.elouastani@gmail.com>
2020-04-27 22:07:41 +0000

    fix

--- eskybe/src/main/java/com/esky/DemoApplication.java
+++ eskybe/src/main/java/com/esky/DemoApplication.java
@@ -3,12 +3,20 @@ package com.esky;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.esky.model.entities")
 @EnableSwagger2
 public class DemoApplication {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
